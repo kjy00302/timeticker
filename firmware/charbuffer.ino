cf44795b2edabd1ec8d9e5c0efb6781ff84726e7
kjy00302 <kjy00302@gmail.com>
2020-11-26 16:35:17 +0900

    최종 펌웨어: 글자 버퍼 구현

---
+++ firmware/charbuffer.ino
@@ -0,0 +1,34 @@
+unsigned int charbuffer_buf[32] = {0,};
+unsigned char charbuffer_readptr = 0;
+unsigned char charbuffer_writeptr = 0;
+
+char charbuffer_isfull(){
+  if (((charbuffer_writeptr + 1) % 32) == charbuffer_readptr){
+    return true;
+  }
+  return false;
+}
+
+char charbuffer_isempty(){
+  if (charbuffer_writeptr == charbuffer_readptr){
+    return true;
+  }
+  return false;
+}
+
+void charbuffer_enqueue(unsigned int chr){
+  if (charbuffer_isfull()){
+    return;
+  }
+  charbuffer_buf[charbuffer_writeptr] = chr;
+  charbuffer_writeptr = (charbuffer_writeptr + 1) % 32;
+}
+
+unsigned int charbuffer_dequeue(){
+  if (charbuffer_isempty()){
+    return;
+  }
+  unsigned int t = charbuffer_buf[charbuffer_readptr];
+  charbuffer_readptr = (charbuffer_readptr + 1) % 32;
+  return t;
+}
