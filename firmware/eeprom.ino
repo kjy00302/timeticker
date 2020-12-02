bb819c1c9576de2da1eefaf4ea0df41f6fd733b2
kjy00302 <kjy00302@gmail.com>
2020-12-02 20:50:53 +0900

    최종 펌웨어: EEPROM 쓰기 관련 함수 추가

--- firmware/eeprom.ino
+++ firmware/eeprom.ino
@@ -10,3 +10,17 @@ void eeprom_read(unsigned int addr, char* data, char len){
     data[i] = Wire.read();
   }
 }
+
+
+void eeprom_write(unsigned int addr, char* data, int len) {
+  Wire.beginTransmission(0x50);
+  Wire.write((addr >> 8));
+  Wire.write((addr & 0xff));
+  Wire.write(data, len);
+  Wire.endTransmission();
+}
+
+char eeprom_isready(){
+  Wire.beginTransmission(0x50);
+  return Wire.endTransmission() == 0;
+}
