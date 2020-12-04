4e1a166ffbb0a885c5589f75ab0c726a70fe5001
kjy00302 <kjy00302@gmail.com>
2020-12-04 18:02:42 +0900

    최종 펌웨어: 사용자 정의 문자
    0xf000 ~ 0xf0ff (256자): 너비가 8인 문자, EEPROM 0x4000부터
    
    0xf100 ~ 0xf17f (128자): 너비가 16인 문자, EEPROM 0x6000부터

--- firmware/font.ino
+++ firmware/font.ino
@@ -1,4 +1,6 @@
 const int HAN_OFFSET = 4096;
+const int CUSTOM_16_OFFSET = 16384;
+const int CUSTOM_32_OFFSET = 24576;
 
 extern unsigned int display_new_buffer[];
 extern char display_newbuf_left;
@@ -37,10 +39,22 @@ void font_writehan(unsigned int chr){
   font_writetonewbuf32(HAN_OFFSET + offset[2] * 32);
 }
 
+void font_writecustom16(unsigned int chr){
+  font_writetonewbuf16(CUSTOM_16_OFFSET + (chr - 0xf000) * 16);
+}
+
+void font_writecustom32(unsigned int chr){
+  font_writetonewbuf32(CUSTOM_32_OFFSET + (chr - 0xf100) * 32);
+}
+
 void font_write(unsigned int chr){
   if (chr < 256){
     font_writeascii(chr);
   } else if (chr >= 0xac00 && chr < 0xd7a4) {
     font_writehan(chr);
+  } else if (chr >= 0xf000 && chr < 0xf100){
+    font_writecustom16(chr);
+  } else if (chr >= 0xf100 && chr < 0xf180){
+    font_writecustom32(chr);
   }
 }
